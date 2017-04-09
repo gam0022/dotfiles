e2d956ab49c4337c9dbbbe72d69701f30a0de80e
gam0022 <gam0022@gmail.com>
2017-04-10 02:45:22 +0900

    Set ignore .gitignore to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,27 +2,27 @@
 declare DRY_RUN=
 while [[ -n "$1" ]]; do
   case "$1" in
-  -n|--dry-run)
-    DRY_RUN=1
-    shift;;
-  -*)
-    die "unknown option: $1";;
-  *)
-    break;;
+    -n|--dry-run)
+      DRY_RUN=1
+      shift;;
+    -*)
+      die "unknown option: $1";;
+    *)
+      break;;
   esac
 done
 
 cd $(dirname $0)
 for dotfile in .?*; do
   case $dotfile in
-    *.elc | *.swp | *~ | .git | ..)
-        continue
-        ;;
+    *.elc | *.swp | *~ | .git | .gitignore | ..)
+      continue
+      ;;
     *)
-        echo "${PWD}/${dotfile}"
-        if [ ! $DRY_RUN ]; then
-       	  ln -Fis "${PWD}/${dotfile}" $HOME
-        fi
-        ;;
-        esac
+      echo "${PWD}/${dotfile}"
+      if [ ! $DRY_RUN ]; then
+        ln -Fis "${PWD}/${dotfile}" $HOME
+      fi
+      ;;
+  esac
 done
